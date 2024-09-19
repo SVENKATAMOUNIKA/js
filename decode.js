6d9ffec95f66ece765d5c971f4ff8eadb2b401e6
SVENKATAMOUNIKA <suremounika95@gmail.com>
2024-09-19 11:22:13 +0530

    ms

---
+++ decode.js
@@ -0,0 +1,32 @@
+decode push,pop,shift and unshift.
+
+
+//---------push------------------//
+let arr1=["HTML","CSS","JS","C"];
+arr1.push("PYTHON");
+console.log(arr1)
+
+
+//--------------pop----------//
+let arr2=["HTML","CSS","JS","C","PYTHON"];
+arr1.pop("PYTHON");
+console.log(arr2)
+
+
+//-----------shift-----------//
+let arr3=["HTML","CSS","JS","C","PYTHON"];
+arr1.shift("PYTHON");
+console.log(arr3)
+
+
+//--------------unshift--------------//
+let arr4=["HTML","CSS","JS","C","PYTHON"];
+arr1.unshift("REACT JS");
+console.log(arr4)
+
+
+//----------decode---------------//
+
+
+let arr5="my name is so on so";
+console.log(decodeURI(arr5))/
